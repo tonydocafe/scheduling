46995fb4adc74e5b97302c3771166c88b9d2cfd1
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-19 16:30:56 -0300

    Create FuncionarioDTO.java

---
+++ agendamentos/src/main/java/model/DAO/FuncionarioDTO.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author perolanegra
+ */
+public class FuncionarioDTO {
+
+    private int id_usuario;
+    private int id_funcionario;
+            
+    private String nome_usuario;
+
+    public String getSenha_usuario() {
+        return senha_usuario;
+    }
+
+    public void setSenha_usuario(String senha_usuario) {
+        this.senha_usuario = senha_usuario;
+    }
+    private String senha_usuario;
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public String getNome_usuario() {
+        return nome_usuario;
+    }
+
+    public void setNome_usuario(String nome_usuario) {
+        this.nome_usuario = nome_usuario;
+    }
+
+    private String cargo_usuario;
+
+    public void setCargo_usuario(String cargo_usuario) {
+        this.cargo_usuario = cargo_usuario;
+    }
+
+    public String getCargo_usuario() {
+        return cargo_usuario;
+    }
+
+    /**
+     * @return the id_funcionario
+     */
+    public int getId_funcionario() {
+        return id_funcionario;
+    }
+
+    /**
+     * @param id_funcionario the id_funcionario to set
+     */
+    public void setId_funcionario(int id_funcionario) {
+        this.id_funcionario = id_funcionario;
+    }
+
+}
